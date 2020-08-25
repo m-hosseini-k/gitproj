4bc3ba016ec03e5f6493a97d358f304960851713
Mohammadreza <mohammadrezahossseinikermani@gmail.com>
2020-08-24 20:17:53 -0700

    241 commit

--- first.sh
+++ first.sh
@@ -28,11 +28,13 @@ if [[ "$ntype" == "    type:add" ]];then
 	git commit -a -m "$n commit"
 	git push origin master
 elif [[ "$ntype" == "    type:minus" ]]; then
-	n=$(redis-cli -a '123' get n)
-        n-=namount
-        counter=$(redis-cli -a '123' n)
+	echo "ok"
+        n=$(redis-cli -a '123' get n)
+        echo "$n"
+        n=$((n-namount))
+        echo "$n"
         od -An -N1 -i /dev/random >> file1.txt
-        git commit -a -m "$counter commit"
+        git commit -a -m "$n commit"
         git push origin master
     else
 	    echo "wrong"
